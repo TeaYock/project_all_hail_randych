af5bd2185754ef2553fcedcc00f66d1662cd5415
TEA <123117083+TeaYock@users.noreply.github.com>
2023-05-04 18:54:25 +0300

    db connection fixied

--- api.py
+++ api.py
@@ -6,16 +6,11 @@ import psycopg2
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = False
-
-def get_db_connection():
-    host = user = password = db_name = port = ''
-    with open('.env') as dotenv:
-        dotenv = json.load(dotenv)
-        for k, v in dotenv.items():
-            globals()[k] = v
-    connection = psycopg2.connect(host=host, user=user, password=password, dbname=db_name, port=port)
-    connection.autocommit = True
-    return connection
+host = user = password = db_name = port = ''
+with open('.env') as db_info:
+    db_info = json.load(db_info)
+    for k, v in db_info.items():
+        globals()[k] = v
 
 @app.route('/addresses')
 def addresses():
@@ -25,7 +20,8 @@ def addresses():
     house = request.args.get('house', '')
     post_code = request.args.get('post_code', '')
 
-    connection = get_db_connection()
+    connection = psycopg2.connect(host=host, user=user, password=password, dbname=db_name, port=port)
+    connection.autocommit = True
     selected_addresses = address_selection(connection, region, settlement, street, house, post_code)
     connection.close()
     return jsonify(selected_addresses)
