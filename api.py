a0dadbfbda9a7a2329688463f452bb3dd22322a0
TEA <123117083+TeaYock@users.noreply.github.com>
2023-05-08 23:03:24 +0300

    regular expressions fixed

--- api.py
+++ api.py
@@ -16,19 +16,19 @@ with open('db_variables') as db_info:
 @app.route('/addresses')
 def addresses():
     region = request.args.get('region', '')
-    if re.match("^[а-яА-ЯіІїЇёЁ\s]+$", region) == False:
+    if re.match("^[а-яА-ЯіІїЇёЁ ]+$", region) == False:
         return json.dumps('[Address not valid]').encode('utf8')
     settlement = request.args.get('settlement', '')
     if re.match("^[а-яА-ЯіІїЇёЁ]+$", settlement) == False:
         return json.dumps('[Address not valid]').encode('utf8')
     street = request.args.get('street', '')
-    if re.match("^[а-яА-ЯіІїЇёЁ0-9.\s]+$", street) == False:
+    if re.match("^[а-яА-ЯіІїЇёЁ0-9. ]+$", street) == False:
         return json.dumps('[Address not valid]').encode('utf8')
     house = request.args.get('house', '')
-    if re.match("^[а-яА-ЯіІїЇёЁ0-9.\s]+$", house) == False:
+    if re.match("^[а-яА-ЯіІїЇёЁ0-9. ]+$", house) == False:
         return json.dumps('[Address not valid]').encode('utf8')
     post_code = request.args.get('post_code', '')
-    if re.match("^[0-9]{,5}$", post_code) == False:
+    if re.match("^[0-9]{5}$", post_code) == False:
         return json.dumps('[Address not valid]').encode('utf8')
 
     connection = psycopg2.connect(host=host, user=user, password=password, dbname=db_name, port=port)
